af71043646e9749c7b06091a2062141e196b222c
Bárbara <babivaldez@gmail.com>
2024-02-09 16:02:46 -0300

    niciando o projeto

--- app/src/main/java/com/example/trybevirtualmenu/views/MainActivity.kt
+++ app/src/main/java/com/example/trybevirtualmenu/views/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.trybevirtualmenu.R
 
 class MainActivity : AppCompatActivity() {
-
+    // iniciando o projeto
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

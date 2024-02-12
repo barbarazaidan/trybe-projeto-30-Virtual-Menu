548b8be2a4b0623a827f07aece5573430645505c
Bárbara <babivaldez@gmail.com>
2024-02-12 10:52:44 -0300

    tenta entender por que o teste no git falha

--- app/src/main/java/com/example/trybevirtualmenu/views/DishViewHolder.kt
+++ app/src/main/java/com/example/trybevirtualmenu/views/DishViewHolder.kt
@@ -12,7 +12,7 @@ class DishViewHolder(itemView: View, clickListener: IClickEventListener) : Recyc
     fun bind(dish: Dish) {
         // no DishAdapter, configurei o item_menu_layout como RecyclerView. Sendo assim, aqui preciso chamar
         // os itens com os ids que estão presentes no arquivo item_menu_layout. Estava fazendo isso chamando
-        // os ids do activity_menu_item-detail. Aí dava erro, pois o teste não conseguia encontrar os itens
+        // os ids do activity_menu_item-detail. Aí dava erro, pois o teste não conseguia encontrar os itens.
 
         val imageDish: ShapeableImageView? = itemView.findViewById(R.id.item_menu_image)
         val nameDish: MaterialTextView? = itemView.findViewById(R.id.item_menu_name)

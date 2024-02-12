fe1cfe2e48b9d312a65c72474d9d33f4c5a63fbe
Bárbara <babivaldez@gmail.com>
2024-02-12 10:08:55 -0300

    finaliza req03

--- app/src/main/java/com/example/trybevirtualmenu/views/DishViewHolder.kt
+++ app/src/main/java/com/example/trybevirtualmenu/views/DishViewHolder.kt
@@ -10,15 +10,19 @@ import com.google.android.material.textview.MaterialTextView
 
 class DishViewHolder(itemView: View, clickListener: IClickEventListener) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
     fun bind(dish: Dish) {
-        val imageDish: ShapeableImageView? = itemView.findViewById(R.id.detail_image)
-        val nameDish: MaterialTextView? = itemView.findViewById(R.id.detail_name)
-        val descriptionDish: MaterialTextView? = itemView.findViewById(R.id.detail_description)
-        val priceDish: MaterialTextView? = itemView.findViewById(R.id.detail_price)
+        // no DishAdapter, configurei o item_menu_layout como RecyclerView. Sendo assim, aqui preciso chamar
+        // os itens com os ids que estão presentes no arquivo item_menu_layout. Estava fazendo isso chamando
+        // os ids do activity_menu_item-detail. Aí dava erro, pois o teste não conseguia encontrar os itens
+
+        val imageDish: ShapeableImageView? = itemView.findViewById(R.id.item_menu_image)
+        val nameDish: MaterialTextView? = itemView.findViewById(R.id.item_menu_name)
+        // val descriptionDish: MaterialTextView? = itemView.findViewById(R.id.detail_description)
+        // val priceDish: MaterialTextView? = itemView.findViewById(R.id.detail_price)
 
         imageDish?.setImageResource(dish.image)
         nameDish?.text = dish.name
-        descriptionDish?.text = dish.description
-        priceDish?.text = dish.price.toString()
+        // descriptionDish?.text = dish.description
+        // priceDish?.text = dish.price.toString()
     }
 
     init{
--- app/src/main/java/com/example/trybevirtualmenu/views/MenuItemDetailActivity.kt
+++ app/src/main/java/com/example/trybevirtualmenu/views/MenuItemDetailActivity.kt
@@ -1,13 +1,50 @@
 package com.example.trybevirtualmenu.views
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trybevirtualmenu.R
+import com.example.trybevirtualmenu.models.Dish
+import com.example.trybevirtualmenu.models.dishesList
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 
 class MenuItemDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_item_detail)
+
+        val dishId = intent.getIntExtra("dish_id", -1)
+        if (dishId != -1) {
+            val dish = dishesList.find { it.id == dishId }
+            if (dish != null) {
+                updateUIWithDishDetails(dish)
+            } else {
+                print("Prato não encontrado")
+            }
+        } else {
+            print("Id incorreto!")
+            finish()
+        }
+
+        val backButton: MaterialButton = findViewById(R.id.detail_back)
+        backButton.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    private fun updateUIWithDishDetails(dish: Dish) {
+        // Encontrou o prato, agora atualize as visualizações com os detalhes do prato
+
+        val nameDish: TextView = findViewById(R.id.detail_name)
+        val descriptionDish: TextView = findViewById(R.id.detail_description)
+        val priceDish: TextView = findViewById(R.id.detail_price)
+
+        nameDish.text = dish.name
+        descriptionDish.text = dish.description
+        priceDish.text = dish.price.toString()
     }
 }
+
